0a7fa00c54d413287edd0afc96959a0a88f11bf8
dev-splin <dev.splin@gmail.com>
2022-10-19 13:20:56 +0900

    UPDATE : [Vue] View - API 호출

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -5,11 +5,29 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "NewsView"
+  name: "NewsView",
+
+  data() {
+    return {
+      users: [],
+    }
+  },
+
+  created() {
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function (response) {
+        thi
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
